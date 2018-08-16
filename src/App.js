8c60e471329ecd07f005b1bcddcaf3478bd82e1d
Brandon <brandoncyu@gmail.com>
2018-08-16 13:25:59 -0700

    finished unit 75 exercise 2

--- src/App.js
+++ src/App.js
@@ -4,43 +4,34 @@ import CartFooter from './components/CartFooter'
 import CartItems from './components/CartItems'
 import AddItem from './components/AddItem'
 import Total from './components/Total'
+import axios from 'axios'
 
 const copyright = 2016
-
-// const cartItemsList = [
-//   { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-//   { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-//   { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-// ]
-//
-// const addToCart = ({quantity, product}) => {
-//   const index = cartItemsList.length
-//   cartItemsList.push({ id: index, product, quantity })
-//   console.log(cartItemsList)
-// }
-
-const product = [
-  { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-  { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-  { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-  { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-  { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-  { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-  { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-  { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-  { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 }
-]
 
 class App extends Component {
   constructor (){
     super()
     this.state = {
-      cartItemsList: [
-        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-      ]
+      cartItemsList: [],
+      products: []
     }
+  }
+
+  async componentDidMount(){
+    const itemsPull = await axios.get('http://localhost:8082/api/items')
+    const products = await axios.get('http://localhost:8082/api/products')
+
+    let items = itemsPull.data.map(element => {
+      let product = products.data.find(e => e.id === element.product_id)
+      element.product = product
+      return element
+    })
+
+
+    this.setState({
+      cartItemsList: items,
+      products: products.data
+    })
   }
 
   addToCart = ({quantity, product}) => {
@@ -56,7 +47,7 @@ class App extends Component {
         <Header />
         <CartItems cartItemsList={this.state.cartItemsList} />
         <Total cartItemsList={this.state.cartItemsList} />
-        <AddItem productItems={product} addToCart={this.addToCart} />
+        <AddItem productItems={this.state.products} addToCart={this.addToCart} />
         <CartFooter copyright={copyright}/>
       </div>
     );
